df8a22f9fae85cbf346a49fad50173140d994334
Vitaliy <kiryuschin.vitalij@yandex.ru>
2025-06-24 10:29:27 +0300

    Remove unused unit type remove

--- RELEASE_NOTES.md
+++ RELEASE_NOTES.md
@@ -1,7 +1,13 @@
-# 1.0.1
+# Release notes
+
+## 1.1.1
+
+- Removed unused unit return type
+
+## 1.1.0
 
 - Added MEDIUM space_age solutions
 
-# 1.0.0
+## 1.0.0
 
 - Added EASY palindrome_number, remove_duplicates, roman_to_integer, two_sum solutions
--- src/bin/space_age.rs
+++ src/bin/space_age.rs
@@ -1,6 +1,6 @@
 use my_rust_algo::solutions::medium::space_age_simple_approach::{Duration, Earth, Planet};
 
-fn main() -> () {
+fn main() {
     let seconds = 1_000_000_000;
     let duration = Duration::from(seconds);
     let output = Earth::years_during(&duration);
